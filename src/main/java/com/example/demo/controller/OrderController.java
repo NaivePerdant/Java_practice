c24c1963f5fb6e2093de68ccdc1b927436553494
NaivePerdant <1016259419@qq.com>
2020-05-21 19:59:17 +0800

    Rsetful规范化1

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -4,9 +4,7 @@ import com.example.demo.mapper.ItemMapper;
 import com.example.demo.pojo.Order;
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,21 +13,19 @@ import java.util.List;
  * @author perdant
  */
 @RestController
+@RequestMapping(value = "/order")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @Autowired
-    private ItemMapper itemMapper;
-
     /**
      * 查询所有订单信息
      * @return
      */
-    @GetMapping("/queryOrderList")
-    public List<Order> queryOrderList() {
-        return orderService.queryOrderList();
+    @RequestMapping(method = RequestMethod.GET)
+    public String queryOrderList() {
+        return orderService.queryOrderList().toString();
     }
 
     /**
@@ -37,8 +33,27 @@ public class OrderController {
      * @param id
      * @return
      */
-    @GetMapping("/queryById/{id}")
-    public Order queryById(@PathVariable String id) {
-        return orderService.queryById(id);
+    @RequestMapping(method = RequestMethod.GET,value = "/{id}")
+    public String queryById(@PathVariable String id) {
+        return orderService.queryById(id).toString();
     }
+
+    /**
+     * 新增一条订单信息
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public String addOrder(){return " ";}
+
+    /**
+     * 根据id删除一条订单信息
+     * @param id
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.DELETE,value = "/{id}")
+    public String deleteOrder(@PathVariable String id){return " ";}
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public String updateOrder(){return " ";}
+
 }
